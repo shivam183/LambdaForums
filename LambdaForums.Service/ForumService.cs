2267db2d94d9399b4bd83d829aca22b8477066e2
shivam183 <soodshivam1831997@gmail.com>
2019-01-31 19:54:50 -0500

    Added Service Layer

---
+++ LambdaForums.Service/ForumService.cs
@@ -0,0 +1,58 @@
+﻿using LambdaForums.Data;
+using LamdaForums.Data;
+using LamdaForums.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LambdaForums.Service
+{
+    public class ForumService : IForum
+    {
+        private readonly ApplicationDbContext _context;
+        public ForumService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task Create(Forum forum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int forumId)
+        {
+            return _context.Forums.FirstOrDefaultAsync(forum => forum.Id == forumId);
+        }
+
+        public IEnumerable<Forum> GetAll()
+        {
+            return _context.Forums
+                .Include(forum => forum.Posts);
+        }
+
+        public IEnumerable<ApplicationUser> GetAllActiveUsers()
+        {
+            return _context.ApplicationUsers;
+        }
+
+        public Forum GetById(int id)
+        {
+            return _context.Forums
+                .Include(forum=> forum.Posts)
+                .FirstOrDefault(forum => forum.Id == id);
+        }
+
+        public Task UpdateForumDescription(int forumId, string newDescription)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateForumTitle(int forumId, string newTitle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LamdaForums.Data/IForum.cs
@@ -0,0 +1,20 @@
+﻿using LamdaForums.Data.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LamdaForums.Data
+{
+    public interface IForum
+    {
+        Forum GetById(int id);
+        IEnumerable<Forum> GetAll();
+
+        IEnumerable<ApplicationUser> GetAllActiveUsers();
+
+        Task Create(Forum forum);
+        Task Delete(int forumId);
+        Task UpdateForumTitle(int forumId, string newTitle);
+        Task UpdateForumDescription(int forumId, string newDescription);
+
+    }
+}
